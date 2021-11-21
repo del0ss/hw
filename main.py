9e7f3faee766e6e8f2694e52c063ee6056a7610a
Dmitry <65163135+dumm0@users.noreply.github.com>
2021-11-21 18:57:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print('Yes')
-print('No')
+import time
+for i in range(30):
+  if i % 5 == 0:
+	  print("Andrew")
+  time.sleep(1)
+	if i == 1:
+		print("1 second has passed")
+		continue 
+	print(i, " seconds has passed")
