c60fc58dc53dcdad3d020dc5ed5b1b2ef11b3e39
Dmitry <65163135+dumm0@users.noreply.github.com>
2021-11-21 18:58:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import time
 for i in range(30):
   if i % 5 == 0:
-	  print("Andrew")
+	  print("dummo")
   time.sleep(1)
 	if i == 1:
-		print("1 second has passed")
 		continue 
 	print(i, " seconds has passed")
